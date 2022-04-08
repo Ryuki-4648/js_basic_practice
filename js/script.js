35fe14af1d7e352f5e3b76d7531b44140016b34a
yuki <hato_poppo73@yahoo.co.jp>
2022-04-08 12:33:07 +0900

    JavaScript基礎　変数、インクリメント、真偽値、比較演算子、ループ、条件分岐

--- js/script.js
+++ js/script.js
@@ -1,65 +1,195 @@
-/* 
-関数を2回実行する関数 
-関数を受け取る関数　＝　高階関数
+'use strict';
+
+
+// 型を知りたいとき
+let hoge01 = 46;
+let hoge02 = "46";
+console.log(typeof hoge01);
+console.log(typeof hoge02);
+
+
+console.log('▼ 変数');
+
+/**
+ * 変数
+ */
+
+// 一般的に値は変数に代入して用いる
+let height = 1.7; // 変数を宣言する
+let weight = 60;
+let bmi = weight/(height * height);
+console.log(`bmi: ${bmi}`);
+
+// 変数の宣言前に変数を使おうとするとエラーになる
+// console.log(hogehoge);
+// Uncaught ReferenceError: hogehoge is not defined
+
+
+console.log('▼ インクリメントとデクリメント');
+
+/**
+ * インクリメントとデクリメント
 */
-function doTwice(func) {
-  func();
-  func();
-}
+let myAge = 28;
+console.log(myAge);
 
-// helloと出力する関数
-function hello() {
-  console.log('hello');
-}
+myAge++;
+console.log(myAge);
 
-// 関数doTwiceにhello（値）を渡す
-doTwice(hello);
+myAge--;
+console.log(myAge);
 
+let myOld = 28;
+myOld += 5; // myOld = myOld + 5 と同じ意味
+console.log(myOld);
 
-
-function doTwice2(func){
-  func();
-  func();
-}
-doTwice2(function hello2() {
-  console.log('hello2');
-});
+myOld -= 3; // myOld = myOld - 3 と同じ意味
+console.log(myOld);
 
 
+// 変数宣言だけして、代入を行わない場合
+let fuga;
+console.log(`fuga: ${fuga}`); // undefinedになる
 
-/* 
-doTwiceWithValue
-「受け取った関数」に、1度目は「Hello!」を渡して実行、2度目は「I am here!!!」を渡して実行する関数
+console.log('▼ 真偽値');
 
-ここでは「引数messageをひとつ取る関数」が渡されている
-messageの中に「Hello!」が入って実行され、次に「I am here!!!」が入って実行される
+/**
+ * 真偽値
 */
-function doTwiceWithValue(func){
-  func('hello doTwiceWithValue'); // 関数に「値」を渡している
-  func('hello doTwiceWithValue!!!');
-}
-// 引数（message）をひとつ取る関数
-// 受け取ったmessageを表示するだけの関数
-doTwiceWithValue(function(message){
-  console.log(message);
-});
 
+const thisIsTrue = true;
+const thisIsFalse = false;
+console.log(thisIsTrue);
+console.log(thisIsFalse);
 
-/*
-コールバック関数
-広い定義でいうと、単なる「高階関数に渡すための関数」
-自分で直接実行するのではなく、相手に実行してもらう関数のこと
+const oneSide = thisIsTrue || thisIsFalse; // 片方でもtrueなら結果がtrueに 両方がfalseなら結果はfalseに
+const bothSide = thisIsTrue && thisIsFalse; // 両方がtrueなら結果がtrueに、片方でもfalseならfalseに
+console.log(`oneSide: ${oneSide}`);
+console.log(`bothSide: ${bothSide}`);
 
-helloやfunction(message)とかはコールバック関数になる
+console.log('▼ 比較演算子');
+
+/**
+ * 比較演算子
 */
+
+// ===と!==は全ての型の値に対して使用可能
+const comparison1 = 1 === 1; // true
+const comparison2 = 2 !== 3; // true
+const comparison3 = 10 < 100; // true
+console.log(comparison1);
+console.log(comparison2);
+console.log(comparison3);
+
+const x = 5;
+const comparison4 = 1 < x && x < 3; // false
+console.log(comparison4);
 
 
 
 /**
- * setTimeout
- * 「受け取ったコールバック関数」を指定ミリ秒後に実行している
- * setTimeout(func, ms)の形になっている
- */
-setTimeout(function(){
-  console.log('settimeout 2秒後にコンソールに出力される')
-}, 2000);
+ * 例題
+*/
+
+const pi = 3.14;
+const radius = 5;
+const area = radius * radius * pi ;
+console.log(`半径が${radius}の円の面積は${area}になります。`);
+
+
+
+/**
+ * 条件分岐とループ
+*/
+
+const value = 12;
+if(value % 2 === 0) { // 2で割ったときのあまりが「0」のとき＝偶数
+  console.log(`${value}は偶数です。`);
+} else { // それ以外＝奇数
+  console.log(`${value}は奇数です。`);
+}
+
+
+
+/**
+ * Switch文
+*/
+const mode01 = 1;
+if (mode01 === 0) {
+  console.log('モード0です');
+} else if (mode01 === 1) {
+  console.log('モード1です');
+} else if (mode01 === 2) {
+  console.log('モード2です');
+} else {
+  console.log('無効なモードです');
+}
+
+const mode02 = 1;
+switch(mode02) {
+  case 0:
+    console.log('モード0です');
+    break;
+    // switch文を終わる。break命令によって明示的にswitch文を終了させないといけない
+    // break命令を書き忘れると、その下のcase 1まで実行されてしまう（フォールする―）
+  case 1:
+    console.log('モード1です');
+    break;
+  case 2:
+    console.log('モード2です');
+    break;
+  default: // その他の場合(どのcaseにも一致しなかった場合)
+    console.log('無効なモードです');
+}
+
+
+
+/**
+ * while(ループ)　すぐ無限ループになって止まったりする
+*/
+
+let count = 0;
+while(count < 10) {
+  console.log(`while文:今は${count + 1}回目のループです。`);
+  count++;
+
+  // countが5になったら抜け出す
+  if(count === 5) {
+    break;
+  }
+}
+
+let counter = 0;
+while(counter < 10) {
+  console.log(`while文:今は${counter + 1}回目のループです。`);
+  counter++
+  continue; // while文の先頭まで戻る
+  console.log('ここは実行されない');
+}
+
+
+
+/**
+ * for(ループ)
+ * 3つの部品からなる for(変数の初期化; ループ条件; 変数の変化)
+ * 部品のうち好きなものを省略することができる。「;」は省略不可！
+*/
+
+for(let count = 0; count < 10; count++) {
+  console.log(`for文:今は${count+1}回目のカウントです`);
+}
+
+
+/**
+ * 例題
+ * 例題：0から10までの間の数を順番にコンソールに表示。
+ * ただし「0は偶数です」「1は奇数です」などというように、偶数奇数のどちらであるかについても表示。
+*/
+
+for(let num = 0; num <= 10; num++) {
+  if(num % 2 === 0) {
+    console.log(`${num}は偶数です。`);
+  } else {
+    console.log(`${num}は奇数です。`);
+  }
+}
